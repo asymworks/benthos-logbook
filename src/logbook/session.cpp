f30d430622634fc1afaeb78a379ef281d961d0c5
J.P. Krauss <jkrauss@asymworks.com>
2012-02-12 08:20:51 -0800

    Fixed bug in register_loaded

--- src/logbook/session.cpp
+++ src/logbook/session.cpp
@@ -357,8 +357,11 @@ void Session::register_loaded(Persistent::Ptr p)
 {
 	id_key_type key(p->type_info(), p->id());
 	if ((m_idmap.find(key) != m_idmap.end()) && (! m_idmap[key].expired()) && (m_idmap[key].lock() != p))
-		throw std::runtime_error("Stale data detected in Identity Map: %s[%d]",
-			p->type_info()->name(), p->id());
+	{
+		char msg[255];
+		sprintf(msg, "Stale data detected in Identity Map: %s[%ld]", p->type_info()->name(), p->id());
+		throw std::runtime_error(std::string(msg));
+	}
 
 	m_idmap[key] = p;
 }
