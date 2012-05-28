7b1bdf66cb52d303a3d56b3d826b41cf8964a607
J.P. Krauss <jkrauss@asymworks.com>
2012-05-28 06:37:55 -0700

    Added doxygen documentation targets to the CMakeLists

---
+++ doc/mainpage.hpp
@@ -0,0 +1,21 @@
+/*
+ * Main Page Data for Doxygen
+ */
+
+/**
+ * @mainpage
+ *
+ * The Benthos Logbook Library is a C++ class library for manipulating scuba
+ * diving logbook databases stored in the Benthos format.  It exposes dives,
+ * dive sites, dive computers, profiles, gas mixes and more as C++ classes with
+ * a lightweight ORM layer for persistence through SQLite.  Benthos lobgooks
+ * are stored as SQLite databases for interoperability; the schema is provided
+ * with this library and is open for third-party clients.
+ *
+ * The library is organized into three major portions: logging, low-level
+ * SQLite database access and the object-relation mapper.  The logging package
+ * provides a Python-like logging functionality which is used to report
+ * debugging information, and supports logging namespaces with individual
+ * reporting and formatting options.  The database access layer provides C++
+ * wrappers around the SQLite API to facilitate the object-relational mapper.
+ */
